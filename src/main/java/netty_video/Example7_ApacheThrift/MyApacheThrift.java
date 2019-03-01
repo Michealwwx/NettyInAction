46de58f267c394683e56f3c1a3a79c2f729e0de4
wuwx <wwx1995next>
2019-03-01 14:45:48 +0800

    modify package

---
+++ src/main/java/netty_video/Example7_ApacheThrift/MyApacheThrift.java
@@ -0,0 +1,10 @@
+package netty_video.Example7_ApacheThrift;
+
+/**
+ * @ClassName: MyApacheThrift
+ * @Description: Apache Thrift
+ * @Author: wuwx
+ * @Date: 2019-02-27 16:16
+ **/
+public class MyApacheThrift {
+}
---
+++ src/main/java/netty_video/Example8_Grpc/Grpc.java
@@ -0,0 +1,14 @@
+package netty_video.Example8_Grpc;
+
+/**
+ * @ClassName: Grpc
+ * @Description: gprc
+ * @Author: wuwx
+ * @Date: 2019-02-27 16:30
+ **/
+public class Grpc {
+
+
+
+
+}
